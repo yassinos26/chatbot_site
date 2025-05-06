6ce2ac44e84c2b53008bd8f5b5f767aa26180a4c
yassinos26 <yassine.250mellouli@gmail.com>
2025-05-06 11:53:13 +0100

    first

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, request, jsonify
+import google.generativeai as genia
+
+app = Flask(__name__)
+
+API_KEY = "AIzaSyBRJovh1LGCsLN2pH_RYcwWu2itj53UyE8"
+genia.configure(api_key=API_KEY)
+model = genia.GenerativeModel("gemini-1.5-flash-002")
+chat = model.start_chat()
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/chat', methods=['POST'])
+def chat_route():
+    data = request.get_json()
+    user_input = data.get("message", "")
+    if user_input.lower() == "cancel":
+        return jsonify({"response": "Session terminée."})
+    response = chat.send_message(user_input)
+    return jsonify({"response": response.text})
+
+if __name__ == "__main__":
+    print("App is running...")
+    app.run(debug=True)
+
